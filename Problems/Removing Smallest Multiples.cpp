32110b385db66947c20eeee001762345b271817a
Dexter-2005 <pchimanshu02@gmail.com>
2026-03-11 12:50:37 +0530

    11-3 morning

---
+++ Problems/Removing Smallest Multiples.cpp
@@ -0,0 +1,62 @@
+/****************************************************
+ ██████╗ ███████╗██╗  ██╗████████╗███████╗██████╗
+ ██╔══██╗██╔════╝╚██╗██╔╝╚══██╔══╝██╔════╝██╔══██╗
+ ██║  ██║█████╗   ╚███╔╝    ██║   █████╗  ██████╔╝
+ ██║  ██║██╔══╝   ██╔██╗    ██║   ██╔══╝  ██╔══██╗
+ ██████╔╝███████╗██╔╝ ██╗   ██║   ███████╗██║  ██║
+ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+        CF ID : DeXTer-69
+*****************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define fori(i, a, b) for (ll i = a; i < b; i++)
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        long long ans = 0;
+
+        vector<bool> isRemoved(n + 1, false);
+
+        for (ll i = 1; i <= n; i++)
+        {
+
+            for (ll j = i; j <= n; j += i)
+            {
+
+                if (s[j - 1] == '1')
+                    break;
+
+                if (isRemoved[j])
+                    continue;
+                else
+                {
+                    isRemoved[j] = true;
+                    ans += i;
+                }
+            }
+        }
+
+        cout << ans << "\n";
+    }
+}
---
+++ Problems/Scuza.cpp
@@ -0,0 +1,52 @@
+/****************************************************
+ ██████╗ ███████╗██╗  ██╗████████╗███████╗██████╗
+ ██╔══██╗██╔════╝╚██╗██╔╝╚══██╔══╝██╔════╝██╔══██╗
+ ██║  ██║█████╗   ╚███╔╝    ██║   █████╗  ██████╔╝
+ ██║  ██║██╔══╝   ██╔██╗    ██║   ██╔══╝  ██╔══██╗
+ ██████╔╝███████╗██╔╝ ██╗   ██║   ███████╗██║  ██║
+ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+        CF ID : DeXTer-69
+*****************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define fori(i, a, b) for (ll i = a; i < b; i++)
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll n,q;
+        cin>>n>>q;
+        vector<ll> a(n);
+        vector<ll> b(n);
+        fori(i,0,n) cin>>a[i];
+        b[0]=a[0];
+        fori(i,1,n) b[i]=a[i]+b[i-1];
+        fori(i,1,n) a[i]=max(a[i],a[i-1]);
+        while(q--){
+            ll x;
+            cin>>x;
+            if(x<a[0]){
+                cout<<"0 ";
+                continue;
+            }
+            cout<< b[(upper_bound(all(a),x)-a.begin())-1]<<" ";
+        }
+        cout<<"\n";
+
+
+    }
+}
