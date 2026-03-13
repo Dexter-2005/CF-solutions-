9066e6b942573914d1d71f604080447e18fa5282
Dexter-2005 <pchimanshu02@gmail.com>
2026-03-13 23:46:59 +0530

    13-3

--- Problems/Virus.cpp
+++ Problems/Virus.cpp
@@ -14,8 +14,8 @@ using namespace std;
 #define pb push_back
 #define all(x) x.begin(), x.end()
 #define fori(i, a, b) for (ll i = a; i < b; i++)
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
+const ll inf = 1e18;
+const ll mod = 1e9 + 7;
 
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 
@@ -27,6 +27,35 @@ int main()
     ll t = 1;
     cin >> t;
     while (t--)
-    {      
+    {
+        ll n, m;
+        cin >> n >> m;
+
+        vector<ll> a(m);
+        for (ll i = 0; i < m; i++) cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        vector<ll> seg;
+        for (ll i = 0; i < m - 1; i++) seg.pb(a[i + 1] - a[i] - 1);
+        seg.pb(a[0] + n - a[m - 1] - 1);
+
+        sort(seg.rbegin(), seg.rend());
+
+        ll s = 0, d = 0;
+
+        for (ll i = 0; i < seg.size(); i++)
+        {
+            ll x = seg[i] - d * 2;
+            if (x > 0)
+            {
+                s++;
+                x -= 2;
+                if (x > 0) s += x;
+                d += 2;
+            }
+        }
+
+        cout << n - s << "\n";
     }
 }
