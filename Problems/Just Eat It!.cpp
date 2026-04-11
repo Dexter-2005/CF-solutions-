993dde41cc26d326002eb16fddaac3611dd5e4f3
Dexter-2005 <pchimanshu02@gmail.com>
2026-04-11 10:53:47 +0530

    11-4 morning

--- Problems/Just Eat It!.cpp
+++ Problems/Just Eat It!.cpp
@@ -27,7 +27,51 @@ int main()
     cin >> t;
     while (t--)
     {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        fori(i, 0, n) cin >> a[i];
+        ll sum=0;
+        fori(i, 0, n) sum+=a[i];
+        // bool allpos=true;
+        // fori(i, 0, n) if(a[i]<0) allpos=false;
+        // if(allpos){
+        //     cout<<"YES"<<endl;  
+        // }   
+        // else{
+        //     cout<<"NO"<<endl;
+        // }
+        ll maxi=0;
+        vector<ll> pre(n);
+        bool ukmkc=false;
+        pre[0]=a[0];
+        fori(i,1,n) {
+            pre[i]=pre[i-1]+a[i];
+            if(pre[i]<=0) ukmkc=true;
+        }
+        vector <ll> suf(n);
+        suf[n-1]=a[n-1];
+        for(ll i=n-2;i>=0;i--){
+            suf[i]=suf[i+1]+a[i];
+            if(suf[i]<=0) ukmkc=true;
+        }
         
-}
-return 0;
+
+        ll currsum=0;
+        fori(i, 0, n){
+            currsum+=a[i];
+            maxi=max(maxi,currsum);
+            if(currsum<0) currsum=0;
+        }
+        if(!ukmkc && maxi==sum) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+
+      
+
+       
+       
+        
+
+    }
+    return 0;
 }
