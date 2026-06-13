6ff7d665a1437cec44de3b78bc8d45a13fcf7fc6
Dexter-2005 <pchimanshu02@gmail.com>
2026-06-13 20:18:22 +0530

    1213

--- Problems/Adjacent XOR.cpp
+++ Problems/Adjacent XOR.cpp
@@ -24,9 +24,32 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     ll t = 1;
-   // cin >> t;
+    cin >> t;
     while (t--)
     {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        vector<ll> b(n);
+        fori(i, 0, n) cin >> a[i];
+        fori(i, 0, n) cin >> b[i];
+        vector<ll> ans(n);
+        if(a[n-1]!=b[n-1]){
+            cout<<"NO\n";
+            continue;
+        }
+        fori(i, 0, n){
+            ans[i]=a[i]^b[i];
+        }
+        bool temp=true;
+        for(ll i=n-2;i>=0;i--){
+            if(ans[i]!=0 &&(ans[i]!=a[i+1]) && ans[i]!=(a[i+1]^ans[i+1])){
+                temp=false;
+                break;
+            }
+        }
+        if(temp) cout<<"YES\n";
+        else cout<<"NO\n";
     
     }
     return 0;
